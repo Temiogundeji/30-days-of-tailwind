64ac00904895615d8432f11d8273be4b3b870d17
Temiogundejim <yusuffogundeji@gmail.com>
2021-12-01 03:39:35 -0800

    setup purge and darkmode

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.js"],
+  },
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
